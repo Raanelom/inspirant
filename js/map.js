47668a9d966f00dcaede403d762e39f6535d2af9
Tim <code@timid.nl>
2021-12-27 14:21:18 +0100

    switch to mapbox

---
+++ js/map.js
@@ -0,0 +1,10 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiemllbWFpbCIsImEiOiJja3hvcDY1YzAwMXprMnFtdnpsa3JjYWt4In0.lvJu2e6CvSipQs5Uakayzg';
+var map = L.mapbox.map('map')
+    .setView([51.94396451510416, 4.470387454615871], 15)
+    .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
+var marker = L.marker([51.94396451510416, 4.470387454615871], {
+    icon: L.mapbox.marker.icon({
+        'marker-color': '#f86767'
+    }),
+    draggable: true
+}).addTo(map);
